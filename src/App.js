5771a9c360c105f3b6088559d0ea97049887cb89
yaojie <limyaojie93@gmail.com>
2021-08-18 18:11:42 +0800

    Add minimal representation for gboard autocompletion bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {   
+    return (
+      <div className="App">
+        <input value={this.state.value} onChange={this.handleChange} />
+      </div>
+    )
+  }
+
+  handleChange(e) {
+    let value = e.target.value;
+    if (value.length === 4) {
+      value = `${value[0]},${value[1]}${value[2]}${value[3]}`
+    }
+
+    this.setState({ value });
+  }
 }
 
 export default App;
